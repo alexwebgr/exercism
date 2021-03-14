a379de997a45abcd6192cd69e12e54a868195c2a
alexwebgr <websurfer992@gmail.com>
2021-03-14 12:52:52 +0200

    enabled passing tests

--- ruby/luhn/luhn_test.rb
+++ ruby/luhn/luhn_test.rb
@@ -13,19 +13,19 @@ class LuhnTest < Minitest::Test
     refute Luhn.valid?("0")
   end
 
+  def test_a_valid_canadian_sin
+    # skip
+    assert Luhn.valid?("055 444 285")
+  end
+
+  def test_valid_number_with_an_even_number_of_digits
+    skip
+    assert Luhn.valid?("095 245 88")
+  end
+
   def test_a_simple_valid_sin_that_remains_valid_if_reversed
     skip
     assert Luhn.valid?("059")
-  end
-
-  def test_a_simple_valid_sin_that_becomes_invalid_if_reversed
-    # skip
-    refute Luhn.valid?("59")
-  end
-
-  def test_a_valid_canadian_sin
-    skip
-    assert Luhn.valid?("055 444 285")
   end
 
   def test_invalid_canadian_sin
@@ -38,9 +38,9 @@ class LuhnTest < Minitest::Test
     refute Luhn.valid?("8273 1232 7352 0569")
   end
 
-  def test_valid_number_with_an_even_number_of_digits
-    skip
-    assert Luhn.valid?("095 245 88")
+  def test_a_simple_valid_sin_that_becomes_invalid_if_reversed
+    # skip
+    refute Luhn.valid?("59")
   end
 
   def test_valid_strings_with_a_non_digit_included_become_invalid
@@ -71,11 +71,6 @@ class LuhnTest < Minitest::Test
   def test_more_than_a_single_zero_is_valid
     # skip
     assert Luhn.valid?("0000 0")
-  end
-
-  def test_input_digit_9_is_correctly_converted_to_output_digit_9
-    skip
-    assert Luhn.valid?("091")
   end
 
   def test_strings_with_non_digits_is_invalid
