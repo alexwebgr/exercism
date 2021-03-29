1e1d1920cb72e1b9841236e39155871b32a6a68e
alexwebgr <websurfer992@gmail.com>
2021-03-29 11:42:15 +0300

    updated the clock

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -3,34 +3,34 @@ class Clock
   private
 
   def initialize(hour: 0, minute: 0)
-    @total_minutes = hour * 60 + minute
+    @minutes = hour * 60 + minute
   end
 
   def display(minutes)
-    "%02d:%02d" % [(minutes / 60) % 24, minutes % 60]
+    '%02d:%02d' % [(minutes / 60) % 24, minutes % 60]
   end
 
   protected
 
-  attr_accessor :total_minutes
+  attr_accessor :minutes
 
   public
 
-  def ==(object)
-    display(object.total_minutes) == display(total_minutes)
+  def ==(other)
+    display(other.minutes) == display(minutes)
   end
 
-  def -(object)
-    @total_minutes -= object.total_minutes
+  def -(other)
+    @minutes -= other.minutes
     self
   end
 
-  def +(object)
-    @total_minutes += object.total_minutes
+  def +(other)
+    @minutes += other.minutes
     self
   end
 
   def to_s
-    display(total_minutes)
+    display(minutes)
   end
 end
