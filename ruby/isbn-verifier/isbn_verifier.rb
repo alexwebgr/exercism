834c24496d3d2ee6f0d89d27e003a8d0ceaf67e7
alexwebgr <websurfer992@gmail.com>
2021-09-06 18:25:23 +0300

    split up isbn-verifier methods

--- ruby/isbn-verifier/isbn_verifier.rb
+++ ruby/isbn-verifier/isbn_verifier.rb
@@ -1,6 +1,8 @@
+class ParsingError < StandardError; end
+
 class IsbnVerifier
-  START = 10
   ISBN_LENGTH = 10
+  MODULO = ISBN_LENGTH + 1
 
   attr_reader :code
 
@@ -13,25 +15,29 @@ class IsbnVerifier
   end
 
   def valid?
-    return false unless code.gsub(/-/, '').length == ISBN_LENGTH
+    digits_sum = digits.map.with_index { |digit, index| digit.to_i * (ISBN_LENGTH - index) }.sum
+    digits_sum += control_digit(digits_sum)
 
+    (digits_sum % MODULO).zero?
+
+  rescue ParsingError => e
+    false
+  end
+
+  def digits
     digits = code.scan(/[0-9]/)
-    return false if digits.size < ISBN_LENGTH - 1 || digits.empty?
+    raise ParsingError, 'input length has to be 10 characters' if digits.empty? || code.gsub(/-/, '').length != ISBN_LENGTH
+    # we assume that after selecting just numbers there should be 9 characters
+    raise ParsingError, 'input contains invalid characters' if digits.size < ISBN_LENGTH - 1
 
-    dig_sum = digits.map.with_index do |digit, index|
-      multiplier = START - index
-      digit.to_i * multiplier
-    end.sum
+    digits
+  end
 
-    control = code[-1].downcase
-    if control == 'x'
-      remainder = (dig_sum % 11)
-      return false if remainder.zero?
+  def control_digit(digits_sum)
+    return 0 unless code[-1].downcase == 'x'
+    remainder = (digits_sum % MODULO)
+    raise ParsingError, 'control digit cannot be zero' if remainder.zero?
 
-      check_digit = 11 - remainder
-      dig_sum += check_digit
-    end
-
-    (dig_sum % 11).zero?
+    MODULO - remainder
   end
 end
